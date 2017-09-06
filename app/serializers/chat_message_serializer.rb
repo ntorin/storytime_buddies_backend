ba336fc63d4b410c994f32fb616215b0f72ee2f0
Iris Inami <irisinami@Iriss-Mac.local>
2017-09-06 07:39:28 -0700

    updating chat_message serializer

--- app/serializers/chat_message_serializer.rb
+++ app/serializers/chat_message_serializer.rb
@@ -1,3 +1,3 @@
 class ChatMessageSerializer < ActiveModel::Serializer
-  attributes :id, :message, :sender_id, :recipient_id
+  attributes :id, :message, :sender_id, :recipient_id, :connection_id, :username, :created_at, :updated_at
 end
